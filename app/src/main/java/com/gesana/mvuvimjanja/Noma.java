66696691b1d49408899f45f8a5a8abf29ddd4342
root <egesaagoya@gmail.com>
2022-11-19 07:31:04 +0300

    splash

---
+++ app/src/main/java/com/gesana/mvuvimjanja/Noma.java
@@ -0,0 +1,14 @@
+package com.gesana.mvuvimjanja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Noma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noma);
+    }
+}
